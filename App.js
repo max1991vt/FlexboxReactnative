d41fd1a78a5659ef0039bb36782a0d278934260d
huy hoang <hoangtronghuy91@gmail.com>
2021-03-17 00:49:12 +0700

    hinh 1

--- App.js
+++ App.js
@@ -5,64 +5,45 @@ export default class App extends Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <View style={{flex: 1}}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'red',
-            }}>
-            <Text>A</Text>
+        <View style={styles.top}>
+          <View style={styles.column1}>
+            <Text style={styles.text}>1</Text>
           </View>
-          <View
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>B</Text>
-          </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'green',
-            }}>
-            <Text>C</Text>
-          </View>
-          <View
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>D</Text>
+          <View style={styles.topColumn2}>
+            <View style={styles.topRow3}>
+              <Text>3</Text>
+            </View>
+            <View style={styles.topRow4}>
+              <Text>4</Text>
+            </View>
           </View>
         </View>
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'blue',
-            }}>
-            <Text>A</Text>
+        {/* bot   */}
+        <View style={styles.bot}>
+          {/* cl1  */}
+          <View style={styles.column1}>
+            <Text style={styles.text}>2</Text>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text>B</Text>
+          {/* cl2  */}
+          <View style={styles.botColumn2}>
+            <View style={styles.topRow5}>
+              <Text>5</Text>
+            </View>
+            <View style={styles.topRow6}>
+              <Text>6</Text>
+            </View>
           </View>
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: 'green',
-            }}>
-            <Text>C</Text>
-          </View>
-          <View
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>D</Text>
+          {/* cl3  */}
+          <View style={styles.botColumn3}>
+            <View style={styles.topRow7}>
+              <Text>7</Text>
+            </View>
+            <View style={styles.topRow8}>
+              <Text>8</Text>
+            </View>
+            <View style={styles.topRow9}>
+              <Text>9</Text>
+            </View>
           </View>
         </View>
       </SafeAreaView>
@@ -71,5 +52,91 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, flexDirection: 'column', alignItems: 'stretch'},
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+  },
+  top: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'black',
+  },
+  column1: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#21d128',
+  },
+  topColumn2: {
+    flex: 0.8,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    backgroundColor: 'red',
+  },
+  topRow3: {
+    flex: 0.5,
+    backgroundColor: '#fe2200',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topRow4: {
+    flex: 0.5,
+    backgroundColor: '#d78e87',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  //bot
+  bot: {
+    flex: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'black',
+  },
+  topRow5: {
+    flex: 0.5,
+    backgroundColor: '#338ab7',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topRow6: {
+    flex: 0.5,
+    backgroundColor: '#6b2966',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topRow7: {
+    flex: 0.5,
+    backgroundColor: '#6f1741',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topRow8: {
+    flex: 0.5,
+    backgroundColor: '#b57626',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  topRow9: {
+    flex: 0.5,
+    backgroundColor: '#9a9098',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botColumn3: {
+    flex: 0.3,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    backgroundColor: 'red',
+  },
+  botColumn2: {
+    flex: 0.5,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    backgroundColor: 'red',
+  },
 });
